7e8c429d4c62302515bc4ba3adf4fb255ca61544
Benjamin Cheng <bencheng200@gmail.com>
2020-11-23 02:57:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,9 @@
 require_relative "./user.rb"
 class Student < User
-    attr_accessor :arr
+    attr_accessor :knowledge
 
     def initialize
-      @arr = []
+      @knowledge = []
     end
 
 end
